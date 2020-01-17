de5f360d409a8c64faf09f2b36fa44a0d79b4f9f
Alexander Sierra <alexandercsierra@gmail.com>
2020-01-17 13:08:07 -0500

    finalized graph stretch

--- client/src/components/Graph.js
+++ client/src/components/Graph.js
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom";
 import Chart from "react-google-charts";
 
 
-class Graph extends React.Component {
-  render() {
-    console.log("players from graph", this.props.players);
+const Graph = (props) => {
+
+
+
+
+    console.log("players from graph", props.players);
     const countryObj = {USA: 0, Brazil: 0, Netherlands: 0, France: 0, Italy: 0}
-    const countries = this.props.players.map(player => {
+    const countries = props.players.map(player => {
         if (player.country==="United States"){
             countryObj.USA +=1;
         } else if (player.country==="Brazil"){
@@ -25,14 +28,14 @@ class Graph extends React.Component {
 
     const data = [
         ["Year", "Number of Players", { role: "style" }],
-        ["USA", countryObj.USA, "color: gray"],
-        ["Brazil", countryObj.Brazil, "color: #76A7FA"],
-        ["Netherlands", countryObj.Netherlands, "color: blue"],
-        ["France", countryObj.France, "stroke-color: #703593; stroke-width: 4; fill-color: #C5A5CF"],
+        ["USA", countryObj.USA, "color: #ed1e14"],
+        ["Brazil", countryObj.Brazil, "color: #009b3a"],
+        ["Netherlands", countryObj.Netherlands, "color: #0249ce"],
+        ["France", countryObj.France, "stroke-color: #ed2939; stroke-width: 4; fill-color: #002395"],
         [
           "Italy",
           countryObj.Italy,
-          "stroke-color: #871B47; stroke-opacity: 0.6; stroke-width: 8; fill-color: #BC5679; fill-opacity: 0.2"
+          "stroke-color: #009247; stroke-width: 4; fill-color: #e00024;"
         ]
       ];
       
@@ -41,7 +44,6 @@ class Graph extends React.Component {
         <Chart chartType="BarChart" width="100%" height="400px" data={data} />
       </div>
     );
-  }
 }
 
 export default Graph
