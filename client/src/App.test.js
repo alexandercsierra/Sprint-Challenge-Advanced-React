add4a9d9f85b8c867b976536e18c39dfa44a92ea
Alexander Sierra <alexandercsierra@gmail.com>
2020-01-17 11:59:02 -0500

    one test running

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { render, fireEvent, cleanup } from '@testing-library/react';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
+test('does app render', ()=>{
+  const container = render(<App/>);
+})
+
